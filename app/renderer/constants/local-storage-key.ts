cf7eb50044c2a4ddfc54eb697757518d834a6091
bessho-ikuya <56667066+bessho-ikuya@users.noreply.github.com>
2022-01-07 13:12:34 +0900

    feat: shower

--- app/renderer/constants/local-storage-key.ts
+++ app/renderer/constants/local-storage-key.ts
@@ -1,4 +1,5 @@
 export const localStorageKey = {
     "CSV_PASS" : "env.CSV_PASS",
-    "CALC_RESULTS" : "data.CALC_RESULTS"
+    "CALC_RESULTS" : "data.CALC_RESULTS",
+    "API_IP" : "env.API_IP"
 }
