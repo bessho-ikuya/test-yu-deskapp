22b6078124d34559f5722d84ce8825c017e1c1ba
bessho-ikuya <56667066+bessho-ikuya@users.noreply.github.com>
2022-01-07 16:32:31 +0900

    fix

--- app/electron-src/services/calc-service.ts
+++ app/electron-src/services/calc-service.ts
@@ -21,7 +21,7 @@ async function execCalc() {
     console.log('request', request)
     const res = await predict(request);
     // 計算結果をローカルストレージに保存
-    console.log(res)
+    console.log(res.data.detail)
     setStorage(localStorageKey.CALC_RESULTS, res.data)
 }
 
