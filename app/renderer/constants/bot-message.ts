4bea21b69a57957085401a6aa5044be57eb8419d
bessho-ikuya <ikuya8888@outlook.jp>
2022-01-20 11:29:36 +0900

    fix: eval message

--- app/renderer/constants/bot-message.ts
+++ app/renderer/constants/bot-message.ts
@@ -1,6 +1,6 @@
 export const botMessageTemplate = {
     "index.default" : "以下のオーダーをお忘れではないでしょうか？確認中のレセプトとオーダー内容が類似したレセプトを紹介します。",
-    "index.eval" : "評価ありがとうございます！次回から反映させていただきます。",
+    "index.eval" : "次回の判定に反映をさせていただきます。",
     "setting.default" : "設定を変更できます。",
     "setting.success" : "設定を更新しました。",
 }
