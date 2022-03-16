3f8f5550b67593354729361e91add2588c4333d0
bessho-ikuya <ikuya8888@outlook.jp>
2022-03-16 13:29:24 +0900

    feat: アイコンテーマ修正

--- app/renderer/constants/icon-list.ts
+++ app/renderer/constants/icon-list.ts
@@ -1,9 +1,21 @@
 import {icon_001} from '../assets/images/icons/icon_001'
 import {icon_002} from '../assets/images/icons/icon_002'
+import {icon_003} from '../assets/images/icons/icon_003'
+import {icon_004} from '../assets/images/icons/icon_004'
+import {icon_005} from '../assets/images/icons/icon_005'
+import {icon_006} from '../assets/images/icons/icon_006'
+import {icon_007} from '../assets/images/icons/icon_007'
+import {icon_008} from '../assets/images/icons/icon_008'
 
 const iconList = {
     "001" : icon_001,
-    "002" : icon_002
+    "002" : icon_002,
+    "003" : icon_003,
+    "004" : icon_004,
+    "005" : icon_005,
+    "006" : icon_006,
+    "007" : icon_007,
+    "008" : icon_008,
 }
 
 export {iconList}
--- app/renderer/constants/theme-list.ts
+++ app/renderer/constants/theme-list.ts
@@ -2,7 +2,13 @@ import {iconList} from './icon-list'
 
 const themeList = {
     "THEME1" : "#ECE9D8",
-    "THEME2" : "#F1F3FF"
+    "THEME2" : "#F1F3FF",
+    "THEME3" : "#FEF4F5",
+    "THEME4" : "#FFFEE8",
+    "THEME5" : "#EDEDED",
+    "THEME6" : "#B5BCC6",
+    "THEME7" : "#FDF7EB",
+    "THEME8" : "#F2F7FD",
 }
 
 const themeDatas = [
@@ -13,6 +19,30 @@ const themeDatas = [
     {
         value : "THEME2",
         label : "テーマ2",
+    },
+    {
+        value : "THEME3",
+        label : "テーマ3",
+    },
+    {
+        value : "THEME4",
+        label : "テーマ4",
+    },
+    {
+        value : "THEME5",
+        label : "テーマ5",
+    },
+    {
+        value : "THEME6",
+        label : "テーマ6",
+    },
+    {
+        value : "THEME7",
+        label : "テーマ7",
+    },
+    {
+        value : "THEME8",
+        label : "テーマ8",
     }
 ]
 
@@ -21,16 +51,48 @@ const themeIcons = {
         {
             value : "001",
             label : iconList["001"],
-        },
-        {
-            value : "002",
-            label : iconList["002"],
         }
     ],
     "THEME2" : [
         {
             value : "002",
             label : iconList["002"],
+        }
+    ],
+    "THEME3" : [
+        {
+            value : "003",
+            label : iconList["003"],
+        }
+    ],
+    "THEME4" : [
+        {
+            value : "004",
+            label : iconList["004"],
+        }
+    ],
+    "THEME5" : [
+        {
+            value : "005",
+            label : iconList["005"],
+        }
+    ],
+    "THEME6" : [
+        {
+            value : "006",
+            label : iconList["006"],
+        }
+    ],
+    "THEME7" : [
+        {
+            value : "007",
+            label : iconList["007"],
+        }
+    ],
+    "THEME8" : [
+        {
+            value : "008",
+            label : iconList["008"],
         }
     ]
 }
