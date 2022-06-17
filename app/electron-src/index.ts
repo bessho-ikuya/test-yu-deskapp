8691426fdef9724eb5246be529cac49510059a43
bessho-ikuya <ikuya8888@outlook.jp>
2022-06-17 11:51:22 +0900

    fix

--- app/electron-src/index.ts
+++ app/electron-src/index.ts
@@ -51,12 +51,12 @@ app.on('ready', async () => {
         mainWindow.webContents.send("ExecCalcResult", { status: true, message: '' })
       })
       .catch(err => {
-        log.info('api error, ',err)
+        log.info('api error, ',err.body)
         // 失敗シグナル送信
         mainWindow.webContents.send("ExecCalcResult", { status: false, message: err.message })
       })
   }).catch(err => {
-    log.info('front error, ',err)
+    log.info('front error, ',err.body)
     // 失敗シグナル送信
     mainWindow.webContents.send("ExecCalcResult", { status: false, message: err.message })
   })
